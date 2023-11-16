6bd7735a08a026f7dfdd6a02c151758a2cee48dc
Japhet1 <40357151+Japhet1@users.noreply.github.com>
2023-11-16 01:41:29 +0000

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://japh-server.onrender.com',
+        target: 'https://44.226.145.213',
         //http://localhost:5000
         changeOrigin: true,
         //rewrite: (path) => path.replace(/^\/api/, ''),
