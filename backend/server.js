78fc8e3af2fb17fad2dac4fd51c40763b49ba411
Japhet1 <40357151+Japhet1@users.noreply.github.com>
2023-11-16 01:00:34 +0000

    update

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const http = require('http')
-const registerRoutes = require('./api/registerRoutes')
+const registerRoutes = require('./api/register')
 
 
 // express app
