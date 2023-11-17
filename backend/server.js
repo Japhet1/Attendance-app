82d0652f900be6ec505d126753eb4e461d462d4c
Japhet1 <40357151+Japhet1@users.noreply.github.com>
2023-11-17 00:29:26 +0000

    updated

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const http = require('http')
 const registerRoutes = require('./routes/registerRoutes')
+const cors = require('cors');
 
 
 // express app
